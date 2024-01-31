c8e90c65713b4e7810d9775078544f1ccf1edd4a
AlexandreFelippe <alemfelippe@gmail.com>
2024-01-31 15:10:12 -0300

    Finaliza exercícios 02 e 04 - tests

---
+++ app/backend/src/tests/Teamn.test.ts
@@ -0,0 +1,37 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import SequelizeModel from '../database/models/SequelizeTeam';
+import { teams, teamId } from './mocks/Team.mocks';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teams Test', () => {
+  it('should return all teams', async function() {
+    sinon.stub(SequelizeModel, 'findAll').resolves(teams as any);
+    const response = await chai.request(app).get('/teams');
+    expect(response.status).to.be.eq(200);
+    expect(response.body).to.be.deep.eq(teams);
+    sinon.restore();
+  })
+
+  it('should return a book by id', async function() {
+    sinon.stub(SequelizeModel, 'findByPk').resolves(teamId as any);
+    const response = await chai.request(app).get('/teams/5');
+    expect(response.status).to.be.eq(200);
+    expect(response.body).to.be.deep.eq(teamId);
+    sinon.restore();
+  });
+
+  it('should return a not found', async function() {
+    sinon.stub(SequelizeModel, 'findByPk').resolves(null);
+    const response = await chai.request(app).get('/teams/6');
+    expect(response.status).to.be.eq(404);
+    sinon.restore();
+  })
+});
---
+++ app/backend/src/tests/mocks/Team.mocks.ts
@@ -0,0 +1,21 @@
+const teamId = {
+  "id": 5,
+  "teamName": "Cruzeiro"
+}
+
+const teams = [
+  {
+    "id": 1,
+    "teamName": "Avaí/Kindermann"
+  },
+  {
+    "id": 2,
+    "teamName": "Bahia"
+  },
+  {
+    "id": 3,
+    "teamName": "Botafogo"
+  }
+]
+
+export { teamId, teams };
