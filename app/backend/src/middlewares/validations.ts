3772242dc0e11875ab22e17f401cc13d48c3518f
AlexandreFelippe <alemfelippe@gmail.com>
2024-02-01 10:29:59 -0300

    Finaliza exercício 10

--- app/backend/src/middlewares/validations.ts
+++ app/backend/src/middlewares/validations.ts
@@ -3,14 +3,14 @@ import { Request, Response, NextFunction } from 'express';
 export default class Validations {
   public static validateEmailPassword(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
-    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     if (!email) return res.status(400).json({ message: 'All fields must be filled' });
     if (!password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
     if (password.length < 6) return res.status(401).json({ message: 'Invalid email or password' });
     if (!regex.test(email)) {
-      return res.status(400).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
     next();
   }
