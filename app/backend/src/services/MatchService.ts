32d61626d35293330927ddc7dba990956fe3ca8d
AlexandreFelippe <alemfelippe@gmail.com>
2024-02-05 19:28:37 -0300

    Exercício 21 ainda quebra

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -34,9 +34,16 @@ export default class MatchService {
     homeTeamGoals: number;
     awayTeamGoals: number;
   }): Promise<ServiceResponse<IMatch>> {
-    const newMatch = await this.matchModel.createMatch({
-      ...newMatchData,
-      inProgress: true,
+    if (newMatchData.homeTeamId === newMatchData.awayTeamId) {
+      return { status: 'UNPROCESSABLE_ENTITY',
+        data: { message: 'It is not possible to create a match with two equal teams' } };
+    }
+    if (!newMatchData.homeTeamId || !newMatchData.awayTeamId) {
+      return { status: 'NOT_FOUND',
+        data: { message: 'There is no team with such id!' } };
+    }
+
+    const newMatch = await this.matchModel.createMatch({ ...newMatchData, inProgress: true,
     });
 
     return { status: 'CREATED', data: newMatch };
